7e8027deb7bce2c653bb8b9f6cac00c7b3099741
farsm2025 <francoismueller@gmail.com>
2026-08-22 12:37:46 -0700

    Ajoute les photos dans Sanity: sanity/schemaTypes/index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
+import {realization} from "./realization";
+import {seoImage} from "./seoImage";
 import {service} from "./service";
 import {siteSettings} from "./siteSettings";
 import {testimonial} from "./testimonial";
 
-export const schemaTypes = [siteSettings, service, testimonial];
+export const schemaTypes = [seoImage, siteSettings, service, realization, testimonial];

01482f69eb9e5b6d4ec15e59ea4485a6b92b5750
farsm2025 <francoismueller@gmail.com>
2026-08-24 22:42:29 +0200

    Supprime la redirection temporaire du site test

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {remotePatterns: [{protocol: "https", hostname: "cdn.sanity.io"}]},
-  async redirects() {
-    return [
-      {
-        source: "/services/transport-debarras",
-        destination: "/services/transport-et-livraison",
-        statusCode: 301,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
